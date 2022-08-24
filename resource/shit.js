5787afadbd9a044fd15ee9ea9b685bb45af0d732
KLohger <fanimdude32@gmail.com>
2022-08-24 13:48:14 +0200

    :)

--- resource/shit.js
+++ resource/shit.js
@@ -1,7 +1,29 @@
-const chord = new Audio('https://klohger.github.io/resource/media/JAVA_01.mp3');
+
+const music = new Audio('https://klohger.github.io/resource/media/JAVA_01.mp3');
 const wow = document.getElementById('wow');
-chord.loop = true;
-myAudioElement.addEventListener("canplaythrough", (event) => {
+wow.style.color = "red";
+music.loop = true;
+let musicEnabled = false;
+let musicCanBeEnabled = false;
+
+music.addEventListener("canplaythrough", (event) => {
   /* the audio is now playable; play it if permissions allow */
-  myAudioElement.play();
+  musicCanBeEnabled = true;
+  
 });
+
+function doTheFunny() {
+	
+	if (musicCanBeEnabled) {
+		if(music.paused) {
+			console.log('unpausing music');
+			music.play();
+			wow.style.color = 'green';
+			
+		} else {
+			console.log('pausing music');
+			music.pause();
+			wow.style.color = 'red';
+		}
+	}
+}
