86ba46f4fe1f54e663d0efbc727bee2c50116804
KLohger <fanimdude32@gmail.com>
2022-08-24 17:32:53 +0200

    Made header bar among others

--- resource/music.js
+++ resource/music.js
@@ -1,35 +1,40 @@
+{
+	const music = new Audio('https://klohger.github.io/media/JAVA_01.ogg');
+	music.loop = true;
 
-const music = new Audio('https://klohger.github.io/media/JAVA_01.ogg');
-music.loop = true;
+	let musicEnabled = false;
 
-let musicEnabled = false;
+	let musicPlayable = false;
 
-let musicCanBeEnabled = false;
+	const musicButton = document.getElementById('music-button');
+	const mainTitleStyle = document.getElementById('main-title').style;
 
-const musicButton = document.getElementById('music-button');
-const mainTitleStyle = document.getElementById('main-title').style;
+	music.addEventListener("canplaythrough", (event) => {
+	  /* the audio is now playable; play it if permissions allow */
+	  musicPlayable = true;
+	});
 
-music.addEventListener("canplaythrough", (event) => {
-  /* the audio is now playable; play it if permissions allow */
-  musicCanBeEnabled = true;
-  
-});
+	music.addEventListener("play", (event) => {
+		musicButton.style.color = 'green';
+		musicButton.style.outlineColor = 'green';
+		mainTitleStyle.animationPlayState = 'running';
+	});
+	music.addEventListener("pause", (event) => {
+		musicButton.style.color = 'red';
+		musicButton.style.outlineColor = 'red';
+		mainTitleStyle.animationPlayState = 'paused';
+	});
 
-function doTheFunny() {
-	
-	if (musicCanBeEnabled) {
-		if(music.paused) {
-			
-			music.play();
-			musicButton.style.color = 'green';
-			mainTitleStyle.animationPlayState = 'running';
-			
-		} else {
-			music.pause();
-			musicButton.style.color = 'red';
-			mainTitleStyle.animationPlayState = 'paused';
+	function doTheFunny() {
+		
+		if (musicPlayable) {
+			if(music.paused) {
+				music.play();
+			} else {
+				music.pause();
+			}
 		}
 	}
-}
 
-musicButton.onclick = doTheFunny;
+	musicButton.onclick = doTheFunny;
+}
